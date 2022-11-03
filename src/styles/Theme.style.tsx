2d6c21164e55908f4879c910683cab4e504ea7c9
BiancaVeres <biancaveres93@gmail.com>
2022-11-03 10:20:55 +0100

    formated code

--- src/styles/Theme.style.tsx
+++ src/styles/Theme.style.tsx
@@ -1,14 +1,14 @@
 export const theme = {
   colors: {
-    orange: '#FDA214',
+    orange: "#FDA214",
     lightOrange: "#FFB84A",
-    lightBlue: '#BCCED9',
-    mediumBlue: '#7191A5',
-    blue: '#6395B8',
-    charcoal: '#304859',
-    lightWhite: '#FCFCFC',
-    darkWhite: '#F2F2F2',
-    black: '#152938',
+    lightBlue: "#BCCED9",
+    mediumBlue: "#7191A5",
+    blue: "#6395B8",
+    charcoal: "#304859",
+    lightWhite: "#FCFCFC",
+    darkWhite: "#F2F2F2",
+    black: "#152938",
     whiteGrey: "#DFE7EC",
-  }
+  },
 };
