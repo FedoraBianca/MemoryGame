e780b5c5c800273cf1c514f30fc06827bc5e2716
Fedora <bianca.stan@arnia.ro>
2022-11-03 13:31:33 +0200

    Add typography and the heading component

---
+++ src/stories/Heading.stories.tsx
@@ -0,0 +1,29 @@
+import Heading from "../components/Heading";
+import { IHeading } from "../components/Heading/Heading";
+import { Story } from '@storybook/react';
+
+export default {
+  title: 'Heading',
+  component: Heading,
+}
+
+const Template: Story<IHeading> = (args: any) => <Heading {...args} />;
+
+export const SmallHeading = Template.bind({});
+SmallHeading.args = {
+  size: 'S',
+  children: 'This is a text'
+};
+
+
+export const MediumHeading = Template.bind({});
+MediumHeading.args = {
+  size: 'M',
+  children: 'This is a text'
+};
+
+export const LargeHeading = Template.bind({});
+LargeHeading.args = {
+  size: 'L',
+  children: 'This is a text'
+};
