e96c5966554f49c635f0e7f76dfafc7892e3b755
BiancaVeres <biancaveres93@gmail.com>
2022-11-08 11:12:05 +0100

    added score menu

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,18 @@ import React from "react";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyles } from "./styles/GlobalStyles.style";
 import { theme } from "./styles/Theme.style";
+import Modal from "./components/Modal";
 import Header from "./components/Header";
+import ScoreMenu from "./components/ScoreMenu";
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
       <Header />
+      <Modal>
+        <ScoreMenu />
+      </Modal>
     </ThemeProvider>
   );
 };
---
+++ src/components/ScoreMenu/ScoreMenu.style.tsx
@@ -0,0 +1,94 @@
+import styled from "styled-components";
+import { theme } from "../../styles/Theme.style";
+
+export const ScoreMenuStyle = styled.div`
+  display: grid;
+  align-items: center;
+  gap: 24px;
+  background: ${theme.colors.darkWhite};
+  border-radius: 10px;
+  padding: 32px 24px 24px;
+
+  @media (min-width: 768px) {
+    gap: 40px;
+    padding: 50px 56px 68px;
+  }
+
+  .score-card {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 280px;
+    padding: 12px 16px;
+    margin-bottom: 8px;
+    background: ${theme.colors.whiteGrey};
+    border-radius: 5px;
+
+    &.winner {
+      background: ${theme.colors.black};
+      color: ${theme.colors.lightWhite};
+    }
+
+    &:not(.winner) {
+      .label {
+        color: ${theme.colors.mediumBlue};
+      }
+    }
+  }
+
+  @media (min-width: 768px) {
+    .score-card {
+      width: 542px;
+    }
+  }
+
+  .label {
+    font-size: 0.813rem;
+  }
+
+  .value {
+    font-size: 1.25rem;
+  }
+
+  .btn-container {
+    display: grid;
+    grid-template-rows: 1fr 1fr;
+    gap: 16px;
+
+    @media (min-width: 768px) {
+      grid-template-columns: 1fr 1fr;
+      grid-template-rows: none;
+    }
+
+    > div {
+      height: 48px;
+    }
+
+    h1,
+    h3 {
+      font-size: 1.125rem;
+    }
+  }
+
+  .heading {
+    font-size: 1.5rem;
+    margin-bottom: 9px;
+  }
+
+  @media (min-width: 768px) {
+    .heading {
+      font-size: 3rem;
+      margin-bottom: 16px;
+    }
+  }
+
+  .sub-heading {
+    font-size: 0.875rem;
+  }
+
+  @media (min-width: 768px) {
+    .sub-heading {
+      font-size: 1.125rem;
+    }
+  }
+`;
